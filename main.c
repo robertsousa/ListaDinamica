4bef063d0957de969ebe5363637e33540c2d1d84
Roberto Sousa <robert6257@gmail.com>
2022-06-04 11:12:21 -0300

    Definido estrutura e variaveis

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+//Definindo a estrutura da lista
+struct no{
+    int dado;
+    struct no *proximo;
+};
+
+//Definindo variaveis
+typedef no *ptr_no;
+prt_no lista;
+int op;
+
 
 int main()
 {
